23f5525a322e38f9f3d389a6de1188137fa0137e
Saman Zand Haghighi <samanzandh@gmail.com>
2024-06-13 16:18:18 +0330

    Fix bugs with user authorization and authentication mixins not being applied

--- flex_report/_version.py
+++ flex_report/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.8.4"
+__version__ = "0.8.0"
 
 get_version = lambda: __version__
--- flex_report/mixins.py
+++ flex_report/mixins.py
@@ -303,8 +303,8 @@ class TablePageMixin(PaginationMixin, TemplateObjectMixin):
 
         return self._format_used_filter(col_name, val) or val
 
-    def setup(self, request, *args, **kwargs):
-        super().setup(request, *args, **kwargs)
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
 
         obj = self.template_object
         if not obj:
@@ -367,6 +367,8 @@ class TablePageMixin(PaginationMixin, TemplateObjectMixin):
                     if bool(v)
                 }
             )
+        
+        return response
 
     def get_used_filters(self, cleaned_data):
         return _(" and ").join(
